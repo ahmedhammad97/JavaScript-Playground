f50c715a445eb74bb67ecb4abb58f478f3c1905c
Ahmed Hammad <hammad97official@gmail.com>
2018-06-10 02:33:46 +0200

    Version 3

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-function setName(){
-  var name = document.getElementById("name");
-  if(name.value==""){alert("Please enter a name!");}
+function changeColor(){
   var x = document.getElementById("txt");
-  x.innerHTML = "Welcome " + name.value;
-  name.value = "";
+  x.style.color = randomColor();
+}
+
+function randomColor(){
+  var hex = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
+  var color = "";
+  for(i=0; i<6; i++){
+    color = color + hex[Math.floor(Math.random()*16)];
+  }
+  //console.log(color);
+  return "#"+color;
 }
