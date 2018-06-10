cd23f337b165c7d454bef0529687b0ad4f99ab34
Ahmed Hammad <hammad97official@gmail.com>
2018-06-10 03:06:30 +0200

    fixes bugs

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
-function changeColor(){
+function setName(){
+  var name = document.getElementById("name");
+  if(name.value==""){alert("Please enter a name!");}
   var x = document.getElementById("txt");
-  x.style.color = randomColor();
-}
-
-function randomColor(){
-  var hex = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
-  var color = "";
-  for(i=0; i<6; i++){
-    color = color + hex[Math.floor(Math.random()*16)];
-  }
-  //console.log(color);
-  return "#"+color;
+  x.innerHTML = "Welcome " + name.value;
+  name.value = "";
 }
