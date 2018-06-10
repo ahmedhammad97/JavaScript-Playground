ccb550b43bd3195168f07a8361b67060fbb00c46
Ahmed Hammad <hammad97official@gmail.com>
2018-06-10 15:26:21 +0200

    happy version

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function changeColor(){
   document.body.style.backgroundColor = randomColor();
-  $("#btn").css("background-color",randomColor());
-  $("#btn").css("color",randomColor());
+  var btn = document.getElementById("btn");
+  btn.style.backgroundColor = randomColor();
+  btn.style.color = randomColor();
 }
 
 function randomColor(){
@@ -10,25 +11,10 @@ function randomColor(){
   for(i=0; i<6; i++){
     color = color + hex[Math.floor(Math.random()*16)];
   }
-  //console.log(color);
+  printColor(color);
   return "#"+color;
 }
 
-function getSize(x){
-  var size = x.css("font-size");
-  size = size.slice(0,-2);
-  return size;
-}
-
-function enlarge(){
-  var size  = getSize($("#btn"));
-  size++;
-  $("#btn").css("font-size",size + "px");
-}
-
-function shrink(){
-  if(getSize($("#btn"))<20)return;
-  var size = getSize($("#btn"));
-  size--;
-  $("#btn").css("font-size",size + "px");
+function printColor(color){
+  console.log(color);
 }
